b0fbac4a79ebde81a324e3c110d66f47551a4547
QuellVM <quell2010@yandex.ru>
2018-11-18 23:53:19 +0300

    add Index

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
-
+use Slim\Views\PhpRenderer;
 require 'vendor/autoload.php';
 
 $app = new Slim\App();
+$container = $app->getContainer();
+$container['renderer'] = new PhpRenderer("./templates");
 
 $app->get('/hello/{name}', function ($request, $response, $args) {
-    return $response->getBody()->write("Hello, " . $args['name']);
+    return $this->renderer->render($response, "/hello.php", $args);
 });
+
+  $app->get('/about', function ($request, $response, $args) {
+      return $this->renderer->render($response, "/about.phtml", $args);
+  });
 
 $app->run();
